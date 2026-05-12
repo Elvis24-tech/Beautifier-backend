57b01974c509dc23727e438fd9716787d1eb7c9e
elvis <kariuki.elvis@student.moringaschool.com>
2026-05-12 17:21:33 +0300

    Worked on Auth

--- users/views.py
+++ users/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from .models import User
+from .serializers import RegisterSerializer
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+
+    if serializer.is_valid():
+        user = serializer.save()
+
+        return Response({
+            "message": "User created successfully",
+            "user": serializer.data
+        })
+
+    return Response(serializer.errors, status=400)
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def login(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    try:
+        user_obj = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=404)
+
+    user = authenticate(username=user_obj.username, password=password)
+
+    if user is None:
+        return Response({"error": "Invalid credentials"}, status=400)
+
+    refresh = RefreshToken.for_user(user)
+
+    return Response({
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
+        "user": {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+        }
+    })
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def me(request):
+    user = request.user
+
+    return Response({
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
+    })
