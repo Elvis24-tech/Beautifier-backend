17ac099e7c686b68ed28adcbcec142d716e7e629
elvis <kariuki.elvis@student.moringaschool.com>
2026-05-13 12:10:36 +0300

    Sales Reflectong On The Dashboard Panel

--- orders/urls.py
+++ orders/urls.py
@@ -3,8 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet
 
 router = DefaultRouter()
-
-# ✅ FIX: empty prefix removes /orders/orders/ bug
 router.register(r"", OrderViewSet, basename="orders")
 
 urlpatterns = [
