1249faa0976b5508467b17ec6aa73547cd7ed978
elvis <kariuki.elvis@student.moringaschool.com>
2026-06-23 12:51:41 +0300

    Fixed Registering admins

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,17 +3,32 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    is_admin = serializers.BooleanField(write_only=True, default=False)
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "is_admin",
+        ]
 
     def create(self, validated_data):
+        is_admin = validated_data.pop("is_admin", False)
+
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
         )
+
+        if is_admin:
+            user.is_staff = True
+            user.save()
+
         return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,24 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
-
-User = get_user_model()
 
 
 @api_view(["POST"])
 def register(request):
     serializer = RegisterSerializer(data=request.data)
+
     if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "User created successfully"})
+        user = serializer.save()
+
+        return Response({
+            "message": "User created successfully",
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "is_admin": user.is_staff,
+        })
+
     return Response(serializer.errors, status=400)
 
 
@@ -20,11 +26,10 @@ def register(request):
 @permission_classes([IsAuthenticated])
 def me(request):
     user = request.user
+
     return Response({
         "id": user.id,
+        "username": user.username,
         "email": user.email,
-        "username": user.username
+        "is_admin": user.is_staff,
     })
-
-
-# simple login is handled by JWT (/api/token/)
